4082582adbbb45469ae085d847f306da1be9534d
JuanC28 <65926922+JuanC28@users.noreply.github.com>
2023-04-13 16:41:31 -0500

    Creación de la pagina de iniciar sesión

--- src/app/SetTime/page.jsx
+++ src/app/SetTime/page.jsx
@@ -1,43 +1,59 @@
-'use client';
+"use client";
 import ButtonBack from "../components/buttonBack";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 import { useDispatch, useSelector } from "react-redux";
-import { setLastInterface, setSessionTime } from "../features/answers/answersSlice";
+import {
+  setLastInterface,
+  setSessionTime,
+} from "../features/answers/answersSlice";
 
 export default function Time() {
-      
   const router = useRouter();
 
   const dispatch = useDispatch();
 
-  const oldInterface = useSelector(state => state.answers.lastInterface);
+  const oldInterface = useSelector((state) => state.answers.lastInterface);
 
   const changeInterface = () => {
-    if (oldInterface != '/SetBook') {
-      router.push('/');
-    }
-    else {
+    if (oldInterface != "/SetBook") {
+      router.push("/");
+    } else {
       router.push(oldInterface);
     }
-    dispatch(setLastInterface('/SetTime'));
-  }
+    dispatch(setLastInterface("/SetTime"));
+  };
 
   return (
     <>
       <div className="flex justify-start mt-10 ml-8">
-        <ButtonBack texto={'Volver'} onClick={() => {changeInterface();}} />
+        <ButtonBack
+          texto={"Volver"}
+          onClick={() => {
+            changeInterface();
+          }}
+        />
       </div>
       <section className="flex flex-col items-center ">
-        <h1 className="mt-15 mb-7 font-bold w-auto text-5xl">¿Cuánto tiempo vas a estudiar?</h1>
+        <h1 className="mt-15 mb-7 font-bold w-auto text-5xl">
+          ¿Cuánto tiempo vas a estudiar?
+        </h1>
         <div className="w-96 mb-12 h-px border border-t border-solid border-gray-line p-0 mx-auto"></div>
-        <select className="mt-5 mb-10 bg-white focus:outline-none text-sky-blue font-bold py-2 px-4 rounded-xl text-xl border w-80 border-blue-text-button h-12 shadow-sm shadow-purple-text" onChange={e => dispatch(setSessionTime(+e.target.value))}>
+        <select
+          className="mt-5 mb-10 bg-white focus:outline-none text-sky-blue font-bold py-2 px-4 rounded-xl text-xl border w-80 border-blue-text-button h-12 shadow-sm shadow-purple-text"
+          onChange={(e) => dispatch(setSessionTime(+e.target.value))}
+        >
           <option value="1">1 hora</option>
           <option value="2">2 horas</option>
           <option value="3">3 horas</option>
           <option value="4">4 horas</option>
           <option value="5">5 horas</option>
         </select>
-        <ButtonBack texto='Continuar' onClick={()=> {router.push('/StudyPanel');}} />
+        <ButtonBack
+          texto="Continuar"
+          onClick={() => {
+            router.push("/StudyPanel");
+          }}
+        />
       </section>
     </>
   );
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,22 +1,28 @@
-'use client';
+"use client";
 
-import Button from "./components/button.jsx"
+import Button from "./components/button.jsx";
 import ButtonBack from "./components/buttonBack.jsx";
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { INFORMATION } from "./utilities/interfaceInformation.js";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 import { useSelector, useDispatch } from "react-redux";
-import { setLastInterface, setSessionTime, setCurrentInfoPrincipalPage, setTimeSystem } from "./features/answers/answersSlice.js";
+import {
+  setLastInterface,
+  setSessionTime,
+  setCurrentInfoPrincipalPage,
+  setTimeSystem,
+} from "./features/answers/answersSlice.js";
 
 export default function Home() {
-
   const dispatch = useDispatch();
 
   const router = useRouter();
 
-  const currentInfo = useSelector(state => state.answers.currentInfoPrincipalPage);
+  const currentInfo = useSelector(
+    (state) => state.answers.currentInfoPrincipalPage
+  );
 
-  const oldInterface = useSelector(state => state.answers.lastInterface);
+  const oldInterface = useSelector((state) => state.answers.lastInterface);
 
   const title = INFORMATION[currentInfo].title;
 
@@ -25,24 +31,30 @@ export default function Home() {
   const goBackButton = INFORMATION[currentInfo].goBackButton;
 
   useEffect(() => {
-    dispatch(setLastInterface(INFORMATION[currentInfo].lastInteface));  
-  })
+    dispatch(setLastInterface(INFORMATION[currentInfo].lastInteface));
+  });
 
   const setGoBackButton = () => {
     if (goBackButton) {
-      return <ButtonBack texto={'Volver'} onClick={() => {changeInterface(oldInterface, false)}}/>;
+      return (
+        <ButtonBack
+          texto={"Volver"}
+          onClick={() => {
+            changeInterface(oldInterface, false);
+          }}
+        />
+      );
     }
     return <div className="ml-8 w-32 h-12"></div>;
-  }
+  };
 
   const changeInterface = (nextInterface, isNewPage) => {
     if (isNewPage) {
       router.push(nextInterface);
-    } 
-    else {
+    } else {
       dispatch(setCurrentInfoPrincipalPage(nextInterface));
     }
-  }
+  };
 
   /*
     Define el tipo el sistema de tiempos de estudio y descanso que se va a utilizar
@@ -58,27 +70,25 @@ export default function Home() {
   */
 
   const updateInformation = (indexButton, nextInterface) => {
-    if (currentInfo == 0) {//Si estamos en la interfaz 0
-      if (indexButton == 1) {//Si pulsamos en los botones 1 o 2
+    if (currentInfo == 0) {
+      //Si estamos en la interfaz 0
+      if (indexButton == 1) {
+        //Si pulsamos en los botones 1 o 2
         changeInterface(nextInterface, true);
         dispatch(setTimeSystem(4)); //Se usara el sistema de tiempos 4
-      }
-      else {
+      } else {
         changeInterface(nextInterface, false);
       }
-    }
-    else if (currentInfo == 1) {
+    } else if (currentInfo == 1) {
       changeInterface(nextInterface, false);
-    }
-    else if (currentInfo == 2) {
+    } else if (currentInfo == 2) {
       if (indexButton == 0) {
         dispatch(setTimeSystem(2));
       } else {
         dispatch(setTimeSystem(3));
       }
       changeInterface(nextInterface, true);
-    }
-    else {
+    } else {
       if (indexButton == 0) {
         dispatch(setTimeSystem(1));
       } else {
@@ -86,23 +96,27 @@ export default function Home() {
       }
       changeInterface(nextInterface, true);
     }
-  }
+  };
 
   return (
     <>
-        <div className="flex justify-start mt-10 ml-8">
-          {setGoBackButton()}
+      <div className="flex justify-start mt-10 ml-8">{setGoBackButton()}</div>
+      <section className="flex flex-col items-center ">
+        <h1 className="mt-15 mb-7 font-bold w-auto text-5xl">{title} </h1>
+        <div className="w-96 mb-12 h-px border border-t border-solid border-gray-line p-0 mx-auto"></div>
+        <div className="flex flex-col gap-5 mb-5">
+          {buttons.map((button, index) => (
+            <Button
+              key={index}
+              texto={button.optionTitle}
+              widht={80}
+              onClick={() => {
+                updateInformation(index, button.nextInterface);
+              }}
+            />
+          ))}
         </div>
-        <section className="flex flex-col items-center ">
-          <h1 className="mt-15 mb-7 font-bold w-auto text-5xl">{title} </h1>
-          <div className="w-96 mb-12 h-px border border-t border-solid border-gray-line p-0 mx-auto"></div>
-          <div className="flex flex-col gap-5 mb-5">
-            {buttons.map((button, index) => (
-              <Button key={index} texto={button.optionTitle} widht={80}
-              onClick={() => {updateInformation(index, button.nextInterface)}} />
-            ))}
-          </div>
-        </section>
+      </section>
     </>
-  )
+  );
 }
